2931b3ab30921805dc59a456c5bbb2652d8ebc0b
Priyanka Kalkat <priyanka.kalkat@itrustmore.com>
2020-08-28 15:15:50 +0530

    Updates

---
+++ precache-manifest.09646833d288aed7c7e45cae444ad10c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d62788f57deae53229598f8bbeef54",
+    "url": "/travelwaale/index.html"
+  },
+  {
+    "revision": "075cc3bd0ee4215b5987",
+    "url": "/travelwaale/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "86b8e61a545b8ff769a4",
+    "url": "/travelwaale/static/css/main.5ce0df41.chunk.css"
+  },
+  {
+    "revision": "075cc3bd0ee4215b5987",
+    "url": "/travelwaale/static/js/2.c41bbf8d.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/travelwaale/static/js/2.c41bbf8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b8e61a545b8ff769a4",
+    "url": "/travelwaale/static/js/main.2e86aa34.chunk.js"
+  },
+  {
+    "revision": "869fe894a4890596e49b",
+    "url": "/travelwaale/static/js/runtime-main.1816f3eb.js"
+  },
+  {
+    "revision": "12ed9d29685b6637aa4952b979380e1e",
+    "url": "/travelwaale/static/media/about.12ed9d29.svg"
+  },
+  {
+    "revision": "b2d9c9278eba229985afca49a3abc80d",
+    "url": "/travelwaale/static/media/banglore.b2d9c927.jpg"
+  },
+  {
+    "revision": "05f7b0a35fd1538b5a7db2e0b76d5f4e",
+    "url": "/travelwaale/static/media/dubai.05f7b0a3.jpg"
+  },
+  {
+    "revision": "0e2275950049921b4986c75b71f68151",
+    "url": "/travelwaale/static/media/france.0e227595.jpg"
+  },
+  {
+    "revision": "d619a9b535b2b944d3933c60ce0b4529",
+    "url": "/travelwaale/static/media/germany.d619a9b5.jpg"
+  },
+  {
+    "revision": "6e419e3f791ad122364bfa00a330880a",
+    "url": "/travelwaale/static/media/japan.6e419e3f.jpg"
+  },
+  {
+    "revision": "8afbc613308e83ac3022493bb92d6d1e",
+    "url": "/travelwaale/static/media/rajasthan.8afbc613.jpg"
+  },
+  {
+    "revision": "0317a73ac9e0b03e176fb9b46aee3078",
+    "url": "/travelwaale/static/media/travelwaalehome.0317a73a.svg"
+  }
+]);
